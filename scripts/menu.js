884bc5ae02e8fa1aeb3998156217b07baf9cbd77
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-24 17:09:49 -0500

    Menu Added

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById('menu-toggle');
-  const menu = document.getElementById('menu');
-
-  menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
-    menuToggle.classList.toggle('active');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+  
+    menuToggle.addEventListener("click", function () {
+      menuToggle.classList.toggle("active");
+      menu.classList.toggle("open");
+    });
+  
+    window.addEventListener("resize", function () {
+      if (window.innerWidth >= 768) {
+        menuToggle.classList.remove("active");
+        menu.classList.remove("open");
+      }
+    });
   });
-});
+  
