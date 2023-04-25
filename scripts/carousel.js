ca572a8ff729f5d4ec4fb2eca50e3e3f49a69102
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-25 17:42:18 -0500

    Carousel Added

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,28 +1,38 @@
 // carousel.js
 
-const carousel = document.querySelector('.carousel');
-let startX;
-let scrollLeft;
-
-function handleSwipeStart(e) {
-  startX = e.pageX || e.touches[0].pageX;
-  scrollLeft = carousel.scrollLeft;
-}
-
-function handleSwipeMove(e) {
-  const currentX = e.pageX || e.touches[0].pageX;
-  const difference = startX - currentX;
-  carousel.scrollLeft = scrollLeft + difference;
-}
-
-carousel.addEventListener('mousedown', handleSwipeStart);
-carousel.addEventListener('mousemove', handleSwipeMove);
-carousel.addEventListener('mouseup', () => {
-  carousel.removeEventListener('mousemove', handleSwipeMove);
-});
-
-carousel.addEventListener('touchstart', handleSwipeStart);
-carousel.addEventListener('touchmove', handleSwipeMove);
-carousel.addEventListener('touchend', () => {
-  carousel.removeEventListener('touchmove', handleSwipeMove);
-});
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('./json/carousel-data.json')
+      .then(response => response.json())
+      .then(data => {
+        const carousel = document.querySelector('.carousel');
+  
+        data.forEach(item => {
+          const card = document.createElement('div');
+          card.classList.add('carousel-card');
+  
+          const img = document.createElement('img');
+          img.src = item.image;
+          img.alt = item.name;
+  
+          const name = document.createElement('h3');
+          name.textContent = item.name;
+  
+          const year = document.createElement('p');
+          year.classList.add('year');
+          year.textContent = item.year;
+  
+          const message = document.createElement('p');
+          message.textContent = item.message;
+  
+          card.appendChild(img);
+          card.appendChild(name);
+          card.appendChild(year);
+          card.appendChild(message);
+          carousel.appendChild(card);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching carousel data:', error);
+      });
+  });
+  
