7442e82bec3d77bb95423f6fbf83e3d77ae06582
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-25 19:37:20 -0500

    Carousel updates

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -3,16 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(response => response.json())
       .then(data => {
         const carousel = document.querySelector('.carousel');
-        const indicatorsContainer = document.querySelector('.carousel-indicators');
   
-        data.forEach((item, index) => {
-          // Create carousel cards
+        data.forEach(item => {
           const card = document.createElement('div');
           card.classList.add('carousel-card');
+          card.addEventListener('click', () => openModal(item));
   
           const img = document.createElement('img');
           img.src = item.image;
           img.alt = item.name;
+  
+          const info = document.createElement('div');
+          info.classList.add('info');
   
           const name = document.createElement('h3');
           name.textContent = item.name;
@@ -25,57 +27,87 @@ document.addEventListener('DOMContentLoaded', function () {
           presentDay.classList.add('presentDay');
           presentDay.textContent = item.presentDay;
   
-          const message = document.createElement('p');
-          message.textContent = item.message;
-  
+          info.appendChild(name);
+          info.appendChild(year);
+          info.appendChild(presentDay);
           card.appendChild(img);
-          card.appendChild(name);
-          card.appendChild(year);
-          card.appendChild(presentDay);
-          card.appendChild(message);
+          card.appendChild(info);
           carousel.appendChild(card);
-  
-          // Create carousel indicators
-          const indicator = document.createElement('div');
-          indicator.classList.add('carousel-indicator');
-          if (index === 0) {
-            indicator.classList.add('current');
-          }
-          indicatorsContainer.appendChild(indicator);
         });
-  
-        // Add event listeners to the carousel and its indicators
-        initializeCarousel(carousel, indicatorsContainer);
       })
       .catch(error => {
         console.error('Error fetching carousel data:', error);
       });
   });
   
-  function initializeCarousel(carousel, indicatorsContainer) {
-    const indicators = indicatorsContainer.querySelectorAll('.carousel-indicator');
-  
-    // Add click event listeners to the indicators
-    indicators.forEach((indicator, index) => {
-      indicator.addEventListener('click', () => {
-        // Scroll to the corresponding card
-        carousel.scrollTo({
-          left: carousel.offsetWidth * index,
-          behavior: 'smooth'
-        });
-      });
+  function openModal(item) {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal || e.target.classList.contains('modal-close')) {
+        modal.remove();
+      }
     });
   
-    // Update the active indicator when scrolling
-    carousel.addEventListener('scroll', () => {
-      const currentIndex = Math.round(carousel.scrollLeft / carousel.offsetWidth);
-      indicators.forEach((indicator, index) => {
-        if (index === currentIndex) {
-          indicator.classList.add('current');
-        } else {
-          indicator.classList.remove('current');
-        }
-      });
-    });
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+  
+    const closeModal = document.createElement('span');
+    closeModal.classList.add('modal-close');
+    closeModal.innerHTML = '&times;';
+    modalContent.appendChild(closeModal);
+  
+    const img = document.createElement('img');
+    img.src = item.image;
+    img.alt = item.name;
+    modalContent.appendChild(img);
+  
+    const name = document.createElement('h3');
+    name.textContent = item.name;
+    modalContent.appendChild(name);
+  
+    const year = document.createElement('p');
+    year.classList.add('year');
+    year.textContent = item.year;
+    modalContent.appendChild(year);
+  
+    const presentDay = document.createElement('p');
+    presentDay.classList.add('presentDay');
+    presentDay.textContent = item.presentDay;
+    modalContent.appendChild(presentDay);
+  
+    const message = document.createElement('p');
+    message.textContent = item.message;
+    modalContent.appendChild(message);
+  
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
   }
+  
+  // Get the carousel and its indicators
+  const carousel = document.querySelector('.carousel');
+  const indicators = document.querySelectorAll('.carousel-indicator');
+  
+  // Add click event listeners to the indicators
+  indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', () => {
+      // Scroll to the corresponding card
+      carousel.scrollTo({
+        left: carousel.offsetWidth * index,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  // Update the active indicator when scrolling
+  carousel.addEventListener('scroll', () => {
+    const currentIndex = Math.round(carousel.scrollLeft / carousel.offsetWidth);
+    indicators.forEach((indicator, index) => {
+      if (index === currentIndex) {
+        indicator.classList.add('current');
+      } else {
+        indicator.classList.remove('current');
+      }
+    });
+  });
   
