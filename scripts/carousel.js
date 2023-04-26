0e6ead317ee6249b2e54b0fd70fbd6efaffd8b34
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-25 19:23:46 -0500

    Alumbi Images

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,12 +1,12 @@
-// carousel.js
-
 document.addEventListener('DOMContentLoaded', function () {
     fetch('./json/carousel-data.json')
       .then(response => response.json())
       .then(data => {
         const carousel = document.querySelector('.carousel');
+        const indicatorsContainer = document.querySelector('.carousel-indicators');
   
-        data.forEach(item => {
+        data.forEach((item, index) => {
+          // Create carousel cards
           const card = document.createElement('div');
           card.classList.add('carousel-card');
   
@@ -20,10 +20,10 @@ document.addEventListener('DOMContentLoaded', function () {
           const year = document.createElement('p');
           year.classList.add('year');
           year.textContent = item.year;
-
+  
           const presentDay = document.createElement('p');
-          year.classList.add('presentDay');
-          year.textContent = item.year;
+          presentDay.classList.add('presentDay');
+          presentDay.textContent = item.presentDay;
   
           const message = document.createElement('p');
           message.textContent = item.message;
@@ -34,38 +34,48 @@ document.addEventListener('DOMContentLoaded', function () {
           card.appendChild(presentDay);
           card.appendChild(message);
           carousel.appendChild(card);
+  
+          // Create carousel indicators
+          const indicator = document.createElement('div');
+          indicator.classList.add('carousel-indicator');
+          if (index === 0) {
+            indicator.classList.add('current');
+          }
+          indicatorsContainer.appendChild(indicator);
         });
+  
+        // Add event listeners to the carousel and its indicators
+        initializeCarousel(carousel, indicatorsContainer);
       })
       .catch(error => {
         console.error('Error fetching carousel data:', error);
       });
   });
-
-  // Get the carousel and its indicators
-const carousel = document.querySelector('.carousel');
-const indicators = document.querySelectorAll('.carousel-indicator');
-
-// Add click event listeners to the indicators
-indicators.forEach((indicator, index) => {
-  indicator.addEventListener('click', () => {
-    // Scroll to the corresponding card
-    carousel.scrollTo({
-      left: carousel.offsetWidth * index,
-      behavior: 'smooth'
+  
+  function initializeCarousel(carousel, indicatorsContainer) {
+    const indicators = indicatorsContainer.querySelectorAll('.carousel-indicator');
+  
+    // Add click event listeners to the indicators
+    indicators.forEach((indicator, index) => {
+      indicator.addEventListener('click', () => {
+        // Scroll to the corresponding card
+        carousel.scrollTo({
+          left: carousel.offsetWidth * index,
+          behavior: 'smooth'
+        });
+      });
     });
-  });
-});
-
-// Update the active indicator when scrolling
-carousel.addEventListener('scroll', () => {
-  const currentIndex = Math.round(carousel.scrollLeft / carousel.offsetWidth);
-  indicators.forEach((indicator, index) => {
-    if (index === currentIndex) {
-      indicator.classList.add('current');
-    } else {
-      indicator.classList.remove('current');
-    }
-  });
-});
-
+  
+    // Update the active indicator when scrolling
+    carousel.addEventListener('scroll', () => {
+      const currentIndex = Math.round(carousel.scrollLeft / carousel.offsetWidth);
+      indicators.forEach((indicator, index) => {
+        if (index === currentIndex) {
+          indicator.classList.add('current');
+        } else {
+          indicator.classList.remove('current');
+        }
+      });
+    });
+  }
   
